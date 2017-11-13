8fc74de52ddc0dd5648133a4336cdbc60dc07bd8
yusufaisal <yusufaisal9@gmail.com>
2017-11-13 14:54:45 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,58 @@
+from math import sqrt
+import csv
+
+# Global Variable
+TestData = []
+LearnData = []
+
+class Data:
+    def __init__(self,nomor,like,provokasi,komentar,emosi,hoax):
+        self.nomor = nomor
+        self.like = like
+        self.provokasi =provokasi
+        self.komentar = komentar
+        self.emosi = emosi
+        self.hoax = hoax
+class ResultClass:
+    def __init__(self, distance, data):
+        self.distance = distance
+        self.data = data
+
+with open('Dataset/data.csv', 'r') as f:
+    reader1 = csv.reader(f)
+    for row in reader1:
+        LearnData.append(Data(row[0], row[1], row[2],row[3],row[4],row[5]))
+with open('Datatrain/data.csv', 'r') as g:
+    reader2 = csv.reader(g)
+    for row in reader2:
+        TestData.append(Data(row[0], row[1], row[2],row[3],row[4],-1))
+
+def Test(Learn_Data, Test_Data):
+    distance  = sqrt((int(Learn_Data.provokasi)-int(Test_Data.provokasi))**2 + (int(Learn_Data.like)-int(Test_Data.like))**2 + (int(Learn_Data.komentar)-int(Test_Data.komentar))**2 + (int(Learn_Data.emosi)-int(Test_Data.emosi))**2)
+    Result = ResultClass(distance, Learn_Data)
+    return Result
+
+def GetClassification(K, Result):
+    Count_Ya = 0
+    Count_Tidak = 0
+    for i in range(K):
+        if (Result[i].data.hoax == "1"):
+            Count_Ya +=1
+        else:
+            Count_Tidak +=1
+
+    # print Count_Ya, Count_Tidak
+    if (Count_Ya > Count_Tidak):
+        return 1
+    else:
+        return 0
+
+if __name__ == '__main__':
+    K = 3000
+
+    for i in range(len(TestData)):
+        Result = []
+        for j in range(len(LearnData)):
+            Result.append(Test(LearnData[j],TestData[i]))
+        Result.sort(key=lambda ResultClass: ResultClass.distance,reverse=False)
+        print (GetClassification(K,Result))
